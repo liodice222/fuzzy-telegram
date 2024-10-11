9a0632902fc3ddde857e32028dda2c15341757d6
lea <115721978+liodice222@users.noreply.github.com>
2024-10-11 13:26:46 -0400

    Update print_env.py

--- print_env.py
+++ print_env.py
@@ -1,14 +1,25 @@
 import os
 
-def print_environment_variables():
-    # Get all environment variables
-    print("TNS_ADMIN:")
-    tns_admin = os.environ.get(TNS_ADMIN)
-    print(tns_admin, "/n")
-    print("DB_USERNAME:")
-    db_username = os.environ.get(DB_USERNAME)
-    print(db_username, "/n")
+def print_environment_variable(var_name):
+    value = os.environ.get(var_name)
+    if value is not None:
+        print(f"{var_name}: {value}")
+    else:
+        print(f"{var_name} is not set.")
 
+def main():
+    # List of environment variables to print
+    env_vars = [
+        'PATH',
+        'DB_USERNAME',
+        'DB_PASSWORD',
+        'DB_DSN',
+        'TNS_ADMIN'
+    ]
+
+    # Print each environment variable
+    for var in env_vars:
+        print_environment_variable(var)
 
 if __name__ == "__main__":
-    print_environment_variables()
+    main()
