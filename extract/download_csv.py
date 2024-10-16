5c2b51870477d039e8766002633d86e7db42e4e3
lea <115721978+liodice222@users.noreply.github.com>
2024-10-16 15:53:30 -0400

    debug Update download_csv.py

--- extract/download_csv.py
+++ extract/download_csv.py
@@ -1,7 +1,7 @@
 import oci
 import os
 
-#TODO: create if statement to make sure files are not already downloaded 
+# TODO: create if statement to make sure files are not already downloaded
 
 def download_csv():
     # Define variables
@@ -22,13 +22,17 @@ def download_csv():
     # Download each CSV file
     for obj in objects:
         if obj.name.endswith('.csv'):
-            print(f"Downloading {obj.name}...")
-            get_obj = object_storage_client.get_object(NAMESPACE, BUCKET_NAME, obj.name)
-            with open(os.path.join(DESTINATION_DIR, obj.name), 'wb') as f:
-                f.write(get_obj.data.content)
+            destination_path = os.path.join(DESTINATION_DIR, obj.name)
+            if not os.path.exists(destination_path):
+                print(f"Downloading {obj.name}...")
+                get_obj = object_storage_client.get_object(NAMESPACE, BUCKET_NAME, obj.name)
+                with open(destination_path, 'wb') as f:
+                    f.write(get_obj.data.content)
+                print(f"Downloaded {obj.name} to {destination_path}")
+            else:
+                print(f"File {obj.name} already exists at {destination_path}, skipping download.")
 
     print("Download completed.")
-
 
 # if __name__ == "__main__":
 #     download_csv()
